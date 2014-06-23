b70e58d32f8e5522d147683bdf007cfec9faf4a7
lanxiaopang <szy1944@hotmail.com>
2014-06-23 19:27:56 +0800

    add Accordion effect

---
+++ work/20140604jncloud/v0.8/js/result.js
@@ -0,0 +1,76 @@
+/*$(function(){
+        *//*根据不同分辨率加载样式文件*//*
+        if($(document).width()>=1024){//宽屏版本
+            document.getElementById('style_link').disabled=true;
+            document.getElementById('style_link_ft').disabled=false;
+        }else{//窄屏版本
+            document.getElementById('style_link_ft').disabled=true;
+            document.getElementById('style_link').disabled=false;
+        }
+
+    }
+)*/
+/*屏幕分辨率变化时根据不同分辨率做不同处理*/
+/*
+$(window).resize(function(){
+    //更换样式文件
+   */
+/* var link_flag = true;
+    if($(document).width()>=1024){//宽屏版本
+        $('#style_link').remove();
+        if(link_flag){
+            $('<link id="style_link_ft" href="css/result_ft.css" rel="stylesheet" />').insertAfter('title');
+            link_flag=false;
+        }
+        }else{//窄屏版本
+        $('#style_link_ft').remove();
+        if(link_flag){
+            $('<link id="style_link_ft" href="css/result.css" rel="stylesheet" />').insertAfter('title');
+            link_flag=false;
+        }
+    }*//*
+
+   */
+/* var a=document.getElementsByTagName('link');
+    if($(document).width()>=1024){//宽屏版本
+
+        a.disabled = true;
+
+    }else{
+
+    }
+
+    function setActiveStyleSheet(title) {
+        var i, a, main;
+        if (title) {
+            for (i = 0; (a = document.getElementsByTagName('link')[i]); i++) {
+                if (a.getAttribute('rel').indexOf('style') != -1
+                    && a.getAttribute('title')) {
+                    a.disabled = true;
+                    if (a.getAttribute('title') == title) a.disabled = false;
+                }
+            }
+        }
+    }
+
+    function getActiveStyleSheet() {
+        var i, a;
+        for (i = 0; (a = document.getElementsByTagName('link')[i]); i++) {
+            if (a.getAttribute('rel').indexOf('style') != -1
+                && a.getAttribute('title')
+                && !a.disabled)
+                return a.getAttribute('title');
+        }
+        return null;
+    }*//*
+
+    if($(document).width()>=1024){//宽屏版本
+        document.getElementById('style_link').disabled=true;
+        document.getElementById('style_link_ft').disabled=false;
+    }else{//窄屏版本
+        alert(document.getElementById('style_link_ft').disabled);
+        document.getElementById('style_link_ft').disabled=true;
+        document.getElementById('style_link').disabled=false;
+    }
+
+})*/
