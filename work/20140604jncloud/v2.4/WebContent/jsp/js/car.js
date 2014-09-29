b6aadecb79bcf3eba35cadd5b95125ec3f6e93cc
lanxiaopang <szy1944@hotmail.com>
2014-09-29 11:29:47 +0800

    increase in 1440 and 1024 two resolutions of css ,and delete some useless files

--- work/20140604jncloud/v2.4/WebContent/jsp/js/car.js
+++ work/20140604jncloud/v2.4/WebContent/jsp/js/car.js
@@ -1,33 +1,25 @@
-
-
+/**
+ *
+ *     本js为车辆档案页car.html配套js
+ *     包括公共重置样式和搜索结果页样式两部分，搜索结果页样式又包括头，尾及中间共三部分
+ *     author：sun mail:szy1944@hotmail.com
+ *     version:1.0
+ *     date:20140929
+ */
 $(function(){
-    /*获取焦点*/
+    /*====获取焦点====*/
     $('#input_box').focus();
 
-    $('#item_basic_table tbody tr:even').addClass('eventr');
-    $('#item_basic_table1 tbody tr:odd').addClass('eventr');
-    $('#item_point_table tbody tr:even').addClass('eventr');
-    $('#item_asj_table tbody tr:odd').addClass('eventr');
-    $('#item_live_table tbody tr:odd').addClass('eventr');
-    $('#item_car_table tbody tr:odd').addClass('eventr');
-    $('#item_card_table tbody tr:odd').addClass('eventr');
 
-    $('.query_log_1').hover(function(){
-        $(this).removeClass('green_color7');
-    },function(){
-        $(this).addClass('green_color7');
-    })
-
-
+    /*====头部云搜索下拉选项效果====*/
     $('#tria').hover(function(){
-            $('#tria1').find('span').removeClass('info_icon_down').addClass('info_icon_up');
-            $('#tria2').slideToggle();
-        },
-        function(){
-            $('#tria1').find('span').removeClass('info_icon_up').addClass('info_icon_down');
-            $('#tria2').slideToggle();
-        });
-
+        $('#tria1').find('span').removeClass('info_icon_down').addClass('info_icon_up');
+        $('#tria2').slideToggle();
+    },
+    function(){
+        $('#tria1').find('span').removeClass('info_icon_up').addClass('info_icon_down');
+        $('#tria2').slideToggle();
+    });
 
     $('#tria2').hover(function(){
         $(this).css('background-color','#f7f7f7');
@@ -46,57 +38,14 @@ $(function(){
     );
 
 
-    /*点击效果*/
-    $(".item_add").find("div:first-child").toggle(
-        function(){
-            if($(this).hasClass("arrow_1")){
-                $(this).removeClass("arrow_1").addClass('arrow_2');
-                $(this).parent().next().slideToggle();
-
-            }
-        },
-        function(){
-            if($(this).hasClass("arrow_2")){
-                $(this).removeClass("arrow_2").addClass('arrow_1');
-                $(this).parent().next().slideToggle();
-            }
-
-        });
-
-
-    $('#sideCatalogBtn').click(function(){
-        $('#sideCatalog').toggle();
-    });
-	if(Sys.ie =='6.0' || Sys.ie =='7.0' || Sys.ie =='8.0'){
-	
-	}
-    else if(Sys.ie =='9.0'){
-        $(window).resize(function(){               //改变大小的时候
-            if($(window).width()<1024){            //判定宽度
-                sc.setAttribute("href","../css/car_1024.css");
-            }else if($(window).width()>1024 && $(window).width()<1200){
-                sc.setAttribute("href","../css/car_1024.css");
-            }else if($(window).width()>=1200 && $(window).width()<1440){
-                sc.setAttribute("href","../css/car_1440.css");
-            }
-        });
-    } else{
-        $(window).resize(function(){               //改变大小的时候
-            if($(window).width()<1024){            //判定宽度
-                sc.setAttribute("href","../css/car_1024.css");
-            }else if($(window).width()>1024 && $(window).width()<1200){
-                sc.setAttribute("href","../css/car_1024.css");
-            }else if($(window).width()>=1200 && $(window).width()<1440){
-                sc.setAttribute("href","../css/car_1440.css");
-            }
-        });
-    }
-
+    /*窗口resize的时候根据不同分辨率加载不同样式*/
+   /* $(window).resize(function(){               //改变大小的时候
+        if($(window).width()<1024){            //判定宽度
+            sc.setAttribute("href","../css/car_1024.css");
+        }else if($(window).width()>1024 && $(window).width()<1200){
+            sc.setAttribute("href","../css/car_1024.css");
+        }else if($(window).width()>=1200 && $(window).width()<1440){
+            sc.setAttribute("href","../css/car_1440.css");
+        }
+    });*/
 })
-
-
-
-
-
-
-
